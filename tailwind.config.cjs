d1fe48d77dbdbc997a856a7a1af51fb6c6613cfb
Laksh Chakraborty <lakshc2010@gmail.com>
2023-01-08 00:00:21 +0530

    feature: added blobs, and basic about me page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        superior: ["superior-title", "serif"],
+        ellograph: ["ellograph-cf", "monospace"],
+      },
+      animation: {
+        blob: "blob 25s infinite ease-in-out",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px)",
+          },
+          "33%": {
+            transform: "translate(35vw, 0px)",
+          },
+          "66%": {
+            transform: "translate(0px, 35vw)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
