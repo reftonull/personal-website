fd2101b1e4b3306c119850a4ca7f4657c1fbf64c
Laksh Chakraborty <lakshc2010@gmail.com>
2023-01-12 03:40:04 +0530

    feature: added some functionality, content: added USM.

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,8 @@ import image from "@astrojs/image";
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 import rehypeRewrite from "rehype-rewrite";
+import remarkGfm from "remark-gfm";
+
 import type { Root, Element } from "hast";
 
 // https://astro.build/config
@@ -19,38 +21,58 @@ import type { Root, Element } from "hast";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx({
-    rehypePlugins: [[rehypeRewrite, {
-      selector: "h1",
-      rewrite: (node: Element, index: number, parent: Element) => {
-        const headingNum = parent.children.filter(value => {
-          return value.type === "element" && (value.tagName === "h1" || value.properties!.className === "md-heading");
-        }).findIndex(value => {
-          return value === node;
-        });
-        if (node.type === "element") {
-          const copy = JSON.parse(JSON.stringify(node));
-          node.tagName = "div";
-          node.properties!.className = "md-heading";
-          const newDiv: Element = {
-            type: "element",
-            tagName: "div",
-            children: [{
-              type: "element",
-              tagName: "div",
-              children: [{
-                type: "text",
-                value: String(headingNum + 1)
-              }]
-            }]
-          };
-          node.children = [newDiv, copy];
-        }
-      }
-    }]]
-  })],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx({
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [
+        [
+          rehypeRewrite,
+          {
+            selector: "h1",
+            rewrite: (node: Element, index: number, parent: Element) => {
+              const headingNum = parent.children
+                .filter((value) => {
+                  return (
+                    value.type === "element" &&
+                    (value.tagName === "h1" ||
+                      value.properties!.className === "md-heading")
+                  );
+                })
+                .findIndex((value) => {
+                  return value === node;
+                });
+              if (node.type === "element") {
+                const copy = JSON.parse(JSON.stringify(node));
+                node.tagName = "div";
+                node.properties!.className = "md-heading";
+                const newDiv: Element = {
+                  type: "element",
+                  tagName: "div",
+                  children: [
+                    {
+                      type: "element",
+                      tagName: "div",
+                      children: [
+                        {
+                          type: "text",
+                          value: String(headingNum + 1),
+                        },
+                      ],
+                    },
+                  ],
+                };
+                node.children = [newDiv, copy];
+              }
+            },
+          },
+        ],
+      ],
+    }),
+  ],
   // output: "server",
   // adapter: vercel()
 });
